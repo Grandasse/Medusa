a1d9af74bcac9c12ba2b9a333d4fc8a55219e314
Grandasse <grandassestl@gmail.com>
2024-02-19 23:48:56 +0100

    add tabs, wip data fill for speakers

---
+++ js/intervenantes.js
@@ -0,0 +1,402 @@
+let interventions = [
+    {
+        "id": 0,
+        "title": "Guillaume Meurice vous répond [EN DIRECT]",
+        "name": "Guillaume Meurice",
+        "date": "2017-05-19",
+        "vues": 1222098,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 1,
+        "title": "Lilian Thuram : football, racisme et géopolitique [EN DIRECT]",
+        "name": "Lilian Thuram",
+        "date": "2017-06-01",
+        "vues": 110044,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 2,
+        "title": "Juan Branco, avocat de Wikileaks [EN DIRECT]",
+        "name": "Juan Branco",
+        "date": "2017-06-20",
+        "vues": 443038,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 3,
+        "title": "Stéphanie Gibaud, Lanceuse d'Alerte SwissLeaks [EN DIRECT]",
+        "name": "Stéphanie Gibaud",
+        "date": "2017-06-20",
+        "vues": 148941,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 4,
+        "title": "Guillaume Ancel, Lieutenant Colonel / Force d'action rapide [EN DIRECT]",
+        "name": "Guillaume Ancel",
+        "date": "2017-06-20",
+        "vues": 475590,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 5,
+        "title": "Benjamin Bayart, Grand Sorcier de l'Internet option vie privée [EN DIRECT]",
+        "name": "Benjamin Bayart",
+        "date": "2017-06-20",
+        "vues": 421204,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 6,
+        "title": "Énergie nucléaire ? José Pluki [EN DIRECT]",
+        "name": "José Pluki",
+        "date": "2017-08-01",
+        "vues": 161745,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 7,
+        "title": "Du Sextoy au \"Dark Web\" : Rayna Stamboliyska [EN DIRECT]",
+        "name": "Rayna Stamboliyska",
+        "date": "2017-08-01",
+        "vues": 156504,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 8,
+        "title": "Étienne Chouard [EN DIRECT]",
+        "name": "Étienne Chouard",
+        "date": "2017-08-04",
+        "vues": 1443524,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 9,
+        "title": "Olivier Delamarche, Analyste financier [EN DIRECT]",
+        "name": "Olivier Delamarche",
+        "date": "2017-09-11",
+        "vues": 538914,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 10,
+        "title": "Philippe Pascot, Corruption et Politique [EN DIRECT]",
+        "name": "Philippe Pascot",
+        "date": "2017-09-11",
+        "vues": 590710,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 11,
+        "title": "Vincent Mignerot : Anticiper l'effondrement ? [EN DIRECT]",
+        "name": "Vincent Mignerot",
+        "date": "2017-09-20",
+        "vues": 458916,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 12,
+        "title": "Bruno Parmentier : Nourrir l'humanité ? [EN DIRECT]",
+        "name": "Bruno Parmentier",
+        "date": "2017-09-29",
+        "vues": 222173,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 13,
+        "title": "Laurent Alexandre : Intelligence artificielle [EN DIRECT]",
+        "name": "Laurent Alexandre",
+        "date": "2017-11-08",
+        "vues": 460614,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 14,
+        "title": "Changement de Civilisation ? Marc Luyckx Ghisi [EN DIRECT]",
+        "name": "Marc Luyckx Ghisi",
+        "date": "2017-11-15",
+        "vues": 2692980,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 15,
+        "title": "Philippe Tixier, Inventeur du Dirigeable du Futur ? [EN DIRECT]",
+        "name": "Philippe Tixier",
+        "date": "2017-12-05",
+        "vues": 39688,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 16,
+        "title": "Cédric Villani : Intelligence artificielle perspectives futures [EN DIRECT]",
+        "name": "Cédric Villani",
+        "date": "2017-12-05",
+        "vues": 649050,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 17,
+        "title": "Frédéric Taddeï : Les limites du débat ? [EN DIRECT]",
+        "name": "Frédéric Taddeï",
+        "date": "2017-12-06",
+        "vues": 625139,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 18,
+        "title": "Les Éconoclastes à l'École de Guerre : Le Jour d'après [EN DIRECT]",
+        "name": "Olivier Delamarche",
+        "date": "2017-12-06",
+        "vues": 205618,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 19,
+        "title": "Les Éconoclastes à l'École de Guerre : Le Jour d'après [EN DIRECT]",
+        "name": "Pierre Sabatier",
+        "date": "2017-12-06",
+        "vues": 2205618,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 20,
+        "title": "Les Éconoclastes à l'École de Guerre : Le Jour d'après [EN DIRECT]",
+        "name": "Nicolas Meilhan",
+        "date": "2017-12-06",
+        "vues": 205618,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 21,
+        "title": "Jean-Marc Jancovici : Anticiper l’effondrement énergétique ? [EN DIRECT]",
+        "name": "Jean-Marc Jancovici",
+        "date": "2017-12-14",
+        "vues": 1194973,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 23,
+        "title": "Périco Légasse : Malbouffe et Mondialisation ? [EN DIRECT]",
+        "name": "Périco Légasse",
+        "date": "2017-12-14",
+        "vues": 174558,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 24,
+        "title": "Pollution atmosphérique : après l'amiante, un nouveau scandale d'Etat ? Solutions ? [EN DIRECT]",
+        "name": "Serge Orru",
+        "date": "2018-01-10",
+        "vues": 83597,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 25,
+        "title": "Pollution atmosphérique : après l'amiante, un nouveau scandale d'Etat ? Solutions ? [EN DIRECT]",
+        "name": "Gilles Dixsaut",
+        "date": "2018-01-10",
+        "vues": 83597,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 26,
+        "title": "Pollution atmosphérique : après l'amiante, un nouveau scandale d'Etat ? Solutions ? [EN DIRECT]",
+        "name": "Nicolas Meilhan",
+        "date": "2018-01-10",
+        "vues": 83597,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 27,
+        "title": "L214 : Exploitation animale & Alimentation - Souffrance du 21ème siècle ? [EN DIRECT]",
+        "name": "Sébastien Arsac",
+        "date": "2018-01-10",
+        "vues": 95003,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 28,
+        "title": "Philippe Bihouix : Le mensonge de la croissance verte ? [EN DIRECT]",
+        "name": "Philippe Bihouix",
+        "date": "2018-01-11",
+        "vues": 765437,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 29,
+        "title": "Propagande VS Journalisme ? RT France, Xenia Fedorova [EN DIRECT]",
+        "name": "Xenia Fedorova",
+        "date": "2018-01-16",
+        "vues": 149314,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 30,
+        "title": "Bernard Stiegler : mutations sociales, politiques, économiques et psychologiques [EN DIRECT]",
+        "name": "Bernard Stiegler",
+        "date": "2018-01-17",
+        "vues": 448024,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 31,
+        "title": "Étienne Klein : Éthique et philosophie des sciences, le rôle des scientifiques ? [EN DIRECT]",
+        "name": "Étienne Klein",
+        "date": "2018-01-17",
+        "vues": 821844,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 32,
+        "title": "La police au service du marché ? Mathieu Rigouste [EN DIRECT]",
+        "name": "Mathieu Rigouste",
+        "date": "2018-02-13",
+        "vues": 188906,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 33,
+        "title": "Journalistes et liberté d'expression en danger ? [EN DIRECT]",
+        "name": "David Koubbi",
+        "date": "2018-02-13",
+        "vues": 82253,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 34,
+        "title": "Journalistes et liberté d'expression en danger ? [EN DIRECT]",
+        "name": "Jean-Pierre Canet",
+        "date": "2018-02-13",
+        "vues": 82253,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 35,
+        "title": "Idriss Aberkane sans filtre [EN DIRECT]",
+        "name": "Idriss Aberkane",
+        "date": "2018-02-15",
+        "vues": 3867765,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 36,
+        "title": "Lord Esperanza : Rap nouvelle génération ? [EN DIRECT]",
+        "name": "Lord Esperanza",
+        "date": "2018-02-15",
+        "vues": 109115,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 37,
+        "title": "Effondrement de la civilisation ? Pablo Servigne [EN DIRECT]",
+        "name": "Pablo Servigne",
+        "date": "2018-02-23",
+        "vues": 1729377,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 38,
+        "title": "Aude Lancelin : Indépendance des médias ? [EN DIRECT]",
+        "name": "Aude Lancelin",
+        "date": "2018-02-27",
+        "vues": 269782,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 39,
+        "title": "Natacha Polony : Journalistes et médias sous contrôles ? [EN DIRECT]",
+        "name": "Natacha Polony",
+        "date": "2018-03-05",
+        "vues": 391516,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 40,
+        "title": "Bernard Friot : Théorie du revenu universel / salaire à la qualification ? [EN DIRECT]",
+        "name": "Bernard Friot",
+        "date": "2018-03-13",
+        "vues": 547538,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 41,
+        "title": "Santé VS Nourriture ? Isabelle Saporta [EN DIRECT]",
+        "name": "Isabelle Saporta",
+        "date": "2018-03-13",
+        "vues": 204681,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 42,
+        "title": "Big Brother habite Place Beauvau ? Exégèse en libertés [EN DIRECT]",
+        "name": "Alexis Fitzjean O Cobhthaigh",
+        "date": "2018-03-30",
+        "vues": 288652,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 43,
+        "title": "Big Brother habite Place Beauvau ? Exégèse en libertés [EN DIRECT]",
+        "name": "Benjamin Bayart",
+        "date": "2018-04-03",
+        "vues": 288652,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 44,
+        "title": "Delamarche, Gave, Sabatier : Géopolitique, macroéconomie sans filtre [EN DIRECT]",
+        "name": "Olivier Delamarche",
+        "date": "2018-04-03",
+        "vues": 1102354,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 45,
+        "title": "Delamarche, Gave, Sabatier : Géopolitique, macroéconomie sans filtre [EN DIRECT]",
+        "name": "Pierre Sabatier",
+        "date": "2018-04-03",
+        "vues": 1102354,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 46,
+        "title": "Delamarche, Gave, Sabatier : Géopolitique, macroéconomie sans filtre [EN DIRECT]",
+        "name": "Charles Gave",
+        "date": "2018-04-03",
+        "vues": 1102354,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 47,
+        "title": "DGSE, Espions, Secrets des Affaires, Crises mondiales. Alain Juillet [EN DIRECT]",
+        "name": "Alain Juillet",
+        "date": "2018-04-07",
+        "vues": 2835340,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 48,
+        "title": "Ambassade de Russie, Espions, Guerre Froide sans filtre [EN DIRECT]",
+        "name": "Artem Studennikov",
+        "date": "2018-04-09",
+        "vues": 360686,
+        "updatedAt": "2024-02-19"
+    },
+    {
+        "id": 49,
+        "title": "Pierre Conesa : Propagande de Guerre, Cinéma, Géopolitique, Opinion publique [EN DIRECT]",
+        "name": "Pierre Conesa",
+        "date": "2018-04-25",
+        "vues": 949658,
+        "updatedAt": "2024-02-19"
+    },
+    // {
+    //     "id": 50,
+    //     "title": "",
+    //     "name": "",
+    //     "date": "2018-04-30",
+    //     "vues": 0,
+    //     "updatedAt": "2024-02-19"
+    // }
+]
--- main.js
+++ main.js
@@ -1,2 +1,3 @@
 computeMaxes();
 createContainers();
+displayIntervenantes();
